8b01219d531e594df06460cf8e7e6062709c8277
Malak <malaksoubai03@gmail.com>
2026-07-13 22:43:45 -0400

    feat(query): add query functions to retreive top-k chunks

--- query.py
+++ query.py
@@ -1,9 +1,11 @@
 # The goals of this script:
 # 1. Query embedding
 # 2. Similarity Search - top-k chunks + similarity score
+# 3. Wire in LlamaIndex 
 
+import config
 
-from chromadb import Knn
+from ingest import load_tools
 
 def embed_query(input: str, embedder) -> list[float]:
     """Embed the query."""
@@ -20,30 +22,61 @@ def embed_query(input: str, embedder) -> list[float]:
 
     print(f"    [STATUS:SUCCESS]  QUERY EMBEDDED.")
 
-    return embedded_query
+    return embedded_query.tolist()
 
 
-def similarity_search(k: int, query_text: str, embedded_query, collection) ->  list[dict]:
+def similarity_search(k: int, embedded_query: list[float], collection) ->  list[dict]:
     """Performs top-k similarity search.
     Prints similarity score of top-k results."""
 
-    if not query_text:
+    if len(embedded_query) == 0:
         print(f"    [STATUS:ERROR]  K is not an acceptable value of range [1, 5].")
         return
-
+    
     if k < 1 or k > 5:
         print(f"    [STATUS:ERROR]  K is not an acceptable value of range [1, 5].")
         return
     
+    # results is a dict with "" keys and [[]] values
     results = collection.query(
-        query_text=query_text,
-        n_result = k,
-        include = ["distances", "metadatas"],
+        query_embeddings=[embedded_query],
+        n_results = k,
+        include = ["distances", "metadatas", "documents"],
     )
+
+    distances = results["distances"][0]
+    metadatas = results["metadatas"][0]
+    documents = results["documents"][0]
+
+    print(f"    [STATUS:SUCCESS]  CHUNKS RETRIEVED.")
+
+    print("-" * 65)
+    print("SIMILARITY SEARCH RESULTS")
+    print("-" * 65)
+    print(f'{"#":<5} {"Filename":<30} {"Page":<5} {"Distance":<10} {"Similarity"}')
+    print("-" * 65)
+
+    for i in range(len(distances)):
+        d = round(distances[i], 4) # rounding only distance would yield to 0 or 1
+        s = round(1 - d, 4)
+        filename = metadatas[i]["filename"]
+        page = metadatas[i]["page"]
+        print(f"{(i + 1):<5} {filename:<30} {page:<5} {d:<10} {s}")
     
-    # Knn(
-    #     query = embedded_query, 
-    #     key = "#embeddings", #K.EMBEDDING ?
-    #     limit = k, 
-    #     return_rank = False
-    # )
+    print(f"Texts used: {documents}\n")
+    
+    return results
+
+
+# SMOKE TEST
+if __name__ == "__main__":
+    print("started SMOKE TESTING query.py file")
+    nlp, embedder, collection = load_tools()
+    queries = [
+        "What ingredients are needed for a chocolate cake recipe?"
+    ]
+    for query in queries:
+        print(f"\n\nQuery: {query}\n")
+        embedded_query = embed_query(query, embedder=embedder)
+        output = similarity_search(2, embedded_query=embedded_query, collection=collection)
+
